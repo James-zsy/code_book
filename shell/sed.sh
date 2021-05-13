1796950373beacb25f293039900203052f84df87
James-zsy <68543629+James-zsy@users.noreply.github.com>
2021-05-13 14:47:38 +0800

    Create sed.sh

---
+++ shell/sed.sh
@@ -0,0 +1,16 @@
+# 2~2p  #偶数行
+# 1~2p
+# 10~2p
+# 3~5p #第3行开始，步进5行
+
+# /finish/p
+
+# 1,20s/finish/You/gp
+
+# 5a\this is a test line
+
+# /^13/i\this is a test line
+
+# s/AAAA/bbbbbbbbbbbbbbbbbbbbbbbbb/g
+# /BBBB/iccccccccccccccccccccccccc
+# /BBBB/=
